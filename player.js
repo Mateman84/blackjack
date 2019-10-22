fe85dc48e52380107f1ec82da4d4387ff5c8ce02
Mattias Englund <mattias.a.englund@gmail.com>
2019-10-22 11:43:43 +0200

    Made a few changes and added player class

---
+++ player.js
@@ -0,0 +1,24 @@
+class Player {
+    constructor(name, hand, wallet) {
+        this.playerName = name;
+        this.playerHand = hand;
+        this.playerWallet = wallet;
+
+        function hit() {
+
+        }
+
+        function stay() {
+
+        }
+
+        function doubleDown() {
+
+        }
+
+        function split() {
+
+        }
+
+    }
+}
