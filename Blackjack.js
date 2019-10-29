63b29fa1df9a0499c8fbecbff523e2e4e542a081
Mattias Englund <mattias.a.englund@gmail.com>
2019-10-29 11:27:23 +0100

    Lagt till .js filer och gjort en del ändringar

--- Blackjack.js
+++ Blackjack.js
@@ -1,8 +1,27 @@
-document.getElementById("submitBtn").onclick(gameStart());
+let playerOne = new Player();
+var button = document.getElementById("startBtn");
 
-function gameStart() {
-    playerOne = new Player();
-    console.log("Funkar det?")
+button.onclick = function gameStart() {
+    var indexOfBetsize = document.getElementById("betSize").selectedIndex;
+    var noOfChips = document.getElementById("betSize").options;
     playerOne.name = document.getElementById("nameCatcher").value;
-    console.log("Name of the player is: " + playerOne.name);
+    playerOne.bet = noOfChips[indexOfBetsize].text;
+    playerOne.hand = 0;
+    if (playerOne.name != "") {
+        console.log("Name of the player is: " + playerOne.name + " and you have: " + playerOne.hand + " cards" + " and you've bet " + playerOne.bet + ".");
+        document.getElementById("playerName").innerHTML = playerOne.name;
+    } else {
+        alert("You need to enter a name!");
+    }
+
 }
+
+function firstCard() {
+    console.log("Your first card is ")
+}
+
+/*function myFunction() {
+  var x = document.getElementById("mySelect").selectedIndex;
+  var y = document.getElementById("mySelect").options;
+  alert("Index: " + y[x].index + " is " + y[x].text);
+}*/
--- card.js
+++ card.js
@@ -1,4 +1,4 @@
-class Player {
+class Card {
     constructor(name, hand, bet) {
         this.playerName = name;
         this.playerHand = hand;
